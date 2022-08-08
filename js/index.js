f9377c941670ec938c456ebfed39741573af409a
bonivan <ibondarev@luxoft.com>
2022-08-08 18:39:11 -0400

    Adding a Message Form

--- js/index.js
+++ js/index.js
@@ -14,10 +14,41 @@ const skills = ['JavaScript', 'HTML', 'CSS', 'SQL'];
 
 const skillsSection = document.getElementById('skills');
 
-skillsList = skillsSection.querySelector('ul'); //???
+skillsList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
     const skill = document.createElement('li'); 
     skill.innerHTML = `${skills[i]}`;
     skillsList.appendChild(skill);
 }
+
+const messageSection = document.getElementById('messages');
+messageSection.hidden = true;
+
+const messageForm = document.getElementsByName('leave_message');
+messageForm.item(0).addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto: ${email}">${name}</a> wrote:  <span>${message}</span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', () => {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    messageList.appendChild(newMessage);
+    newMessage.appendChild(removeButton);
+
+    messageSection.hidden = false;
+
+    messageForm.item(0).reset();
+});
